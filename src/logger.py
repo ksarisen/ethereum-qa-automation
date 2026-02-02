942f18aed797be6eaa01787526b8a2380be072a2
Kerem Sarisen <keremsarisen96@hotmail.com>
2026-02-01 17:35:53 -0800

    windows emoji handler

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ logging.basicConfig(
     level = logging.INFO,
     format =" %(asctime)s [%(levelname)s] %(message)s",
     handlers = [
-        logging.FileHandler("run.log"),
+        logging.FileHandler("run.log", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
